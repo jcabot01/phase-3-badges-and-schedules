e99f5b7c17f04ac14109454a503001af74e0c03d
JonCabot <jfc0053@gmail.com>
2022-05-14 07:09:24 -0500

    completed assignment

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (array)
+  array.map do |name|
+    "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms (speakers)
+  speakers.map.with_index(1) do |speaker, index|
+    "Hello, #{speaker}! You'll be assigned to room #{index}!"
+  end
+end
+
+def printer (array)
+  batch_badge_creator(array).map do |name|
+    puts name
+  end
+  assign_rooms(array).map do |name|
+    puts name
+  end
+end
